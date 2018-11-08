f19a2348a00cd8639fd59c3c21aec72f1ad832bb
Thomas Jolley <tomjolley81@gmail.com>
2018-11-08 15:53:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function takeANumber(line, name) {
   line.push(name)
-  number = line.indexOf(name) + 1
+  var number = line.indexOf(name) + 1
   return `Welcome, ${name}. You are number ${number} in line`
 }
