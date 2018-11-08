a2880658d15db1edc023abc6a5e9b1674ec35358
Thomas Jolley <tomjolley81@gmail.com>
2018-11-08 15:57:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ function takeANumber(line, name) {
   var number = line.indexOf(name) + 1
   return `Welcome, ${name}. You are number ${number} in line.`
 }
+function nowServing(katzDeliLine) {
+  current = katzDeliLine[0]
+  katzDeliLine.slice(1)
+  return current
+  
+}
